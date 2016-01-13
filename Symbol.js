736d413dadadf579d5d4aa8372f48eef9aaca45e
DmitrySigaev <dsigaev@yandex.ru>
2016-01-13 23:26:48 +0300

    How to create a Symbol wrapper object Creating an explicit wrapper object around primitive data types is no longer supported starting with ECMAScript 6.

--- Symbol.js
+++ Symbol.js
@@ -30,3 +30,19 @@ Symbol("foo") === Symbol("foo"); // whatch:	Symbol("foo") === Symbol("foo")	valu
 if (false) // change on true to check the exception
 	var sym = new Symbol(); // TypeError: Symbol is not a constructor
 
+/* 
+ * This prevents authors from creating an explicit Symbol wrapper object instead of a new symbol value. 
+ * Creating an explicit wrapper object around primitive data types is no longer supported starting with
+ * ECMAScript 6. However, existing primitive wrapper objects like new Boolean, new String and
+ * new Number can still be created for legacy reasons.
+ */
+
+/*
+ * And if you really want to create a Symbol wrapper object, you can use the Object() function:
+ */
+
+var sym = Symbol("foo");	//		Whatch:sym			Value:Symbol(foo)	Type:Unknown
+typeof sym;					//		Watch:typeof sym	Value:"symbol"		Type:String
+var symObj = Object(sym);	//		Watch:symObj		Value:{Symbol}		Type:Object
+typeof symObj;				//		Watch:typeof symObj	Value:"object"		Type:String
+

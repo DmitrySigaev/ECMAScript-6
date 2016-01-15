ff6c690e5bb408f7b89d3707c8f726cf74b94d5d
DmitrySigaev <dsigaev@yandex.ru>
2016-01-15 22:45:44 +0300

    Object.getOwnPropertySymbols(obj) The Object.getOwnPropertySymbols() method returns an array of all symbol properties found directly upon a given object.

--- Symbol.js
+++ Symbol.js
@@ -133,3 +133,36 @@ console.log(globalSymIter);
 console.log(Symbol.keyFor(SymIter));
 console.log(Symbol.keyFor(globalSymIter));
 
+
+/* ---------------------------------------------*/
+
+/*
+ * The Object.getOwnPropertySymbols() method returns an array of all symbol properties found directly upon a given object.
+ * 
+ * Syntax
+ *		Object.getOwnPropertySymbols(obj)
+ * Parameters
+ *		obj
+ *			The object whose symbol properties are to be returned.
+ * Description
+ *		Similar to Object.getOwnPropertyNames(), you can get all symbol properties of a given object as an array of 
+ *		symbols. Note that Object.getOwnPropertyNames() itself does not contain the symbol properties of an object 
+ *		and only the string properties.
+ * As all objects have no own symbol properties initially, Object.getOwnPropertySymbols() returns an empty array 
+ * unless you have set symbol properties on your object.
+ */
+
+var obj = {};
+var a = Symbol('a');
+var b = Symbol.for('b');
+
+obj[a] = 'localSymbol';
+obj[b] = 'globalSymbol';
+
+
+var objectSymbols = Object.getOwnPropertySymbols(obj);
+
+console.log(objectSymbols.length); // 2
+console.log(objectSymbols);        // [Symbol(a), Symbol(b)]
+console.log(objectSymbols[0]);     // Symbol(a)
+
